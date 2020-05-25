076264db511518d84822817f23b47543c33cb06e
Adil <adiltruong@umail.ucsb.edu>
2020-05-25 16:15:04 -0700

    Updates

---
+++ precache-manifest.7cbde49f2ad5f42a70acf8afa28e0a6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8e336f4974f5c44ce7dd1782673917",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "0e89ca00792bfdadf1f7",
+    "url": "/react-site/static/css/main.f99b9276.chunk.css"
+  },
+  {
+    "revision": "d56ab5c9bf5d1d8167c0",
+    "url": "/react-site/static/js/2.b2f4a37f.chunk.js"
+  },
+  {
+    "revision": "c5ae140110d7bc08474717a102c40eeb",
+    "url": "/react-site/static/js/2.b2f4a37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e89ca00792bfdadf1f7",
+    "url": "/react-site/static/js/main.4df1633d.chunk.js"
+  },
+  {
+    "revision": "e443f8b832b330e9d110",
+    "url": "/react-site/static/js/runtime-main.dc3bcaa7.js"
+  },
+  {
+    "revision": "d6057904a65ae5c1a295ab6e4e9c3993",
+    "url": "/react-site/static/media/dog1.d6057904.jpg"
+  },
+  {
+    "revision": "d625ad7b54118fcb032ac1c1b480e905",
+    "url": "/react-site/static/media/dog2.d625ad7b.jpg"
+  },
+  {
+    "revision": "5bb87a935ddec3236d58232d92e6433d",
+    "url": "/react-site/static/media/dog3.5bb87a93.jpg"
+  },
+  {
+    "revision": "3032e22184a160c675d52b95e51ba49a",
+    "url": "/react-site/static/media/ear.3032e221.jpg"
+  },
+  {
+    "revision": "9172f3d39c04920a78a18c09631c2266",
+    "url": "/react-site/static/media/me_and_soju.9172f3d3.jpg"
+  },
+  {
+    "revision": "5b832d360191b12fb37ae67de95fada7",
+    "url": "/react-site/static/media/medal.5b832d36.jpg"
+  },
+  {
+    "revision": "0e8a0bd0906dbd2dcbbde565511dfd00",
+    "url": "/react-site/static/media/wh.0e8a0bd0.jpg"
+  }
+]);
